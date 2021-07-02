497c021c80d915f35e03861ab3346309d3a229e6
Minhaz <minhaz725@gmail.com>
2021-07-03 03:52:30 +0600

    initial commit

---
+++ cmake-build-debug/CMakeFiles/RTC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RTC.dir/main.cpp.obj"
+  "RTC.exe"
+  "RTC.exe.manifest"
+  "RTC.pdb"
+  "libRTC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RTC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
